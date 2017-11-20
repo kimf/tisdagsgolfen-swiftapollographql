3b8a04e5674a91485fedd375ed030918adec4447
Kim Fransman <kim.fransman@gmail.com>
2017-11-20 09:17:07 +0100

    Initial Commit

---
+++ Tisdagsgolfen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tisdagsgolfen
+//
+//  Created by Kim Fransman on 2017-11-20.
+//  Copyright © 2017 Kim Fransman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
